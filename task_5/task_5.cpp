1b450463858b74527ea8a0f8e944e8c5468b3889
VadimS20 <vadim_2014_pushkin@mail.ru>
2023-10-09 20:35:12 +0300

    Добавьте файлы проекта.

---
+++ task_5/task_5.cpp
@@ -0,0 +1,14 @@
+﻿#include "task_5.h"
+
+using namespace std;
+
+int main()
+{
+	SP x(new object(24));
+	SP y(new object(37));
+	y->display();
+	x->display();
+	x = y;
+	x->display();
+	return 0;
+}
---
+++ task_5/task_5.h
@@ -0,0 +1,63 @@
+﻿
+#pragma once
+
+#include <iostream>
+
+class object {
+	int data;
+public:
+	object(int d) {
+		data = d;
+	}
+	object(const object& temp) {
+		data = temp.data;
+	}
+	~object(){
+		std::cout << "object has deleted"<<"\n";
+	}
+	object() {
+		data = 0;
+	}
+	int get() {
+		return data;
+	}
+	void display() {
+		std::cout << data<<"\n";
+	}
+};
+
+template<typename T>
+class SP {
+	T* p;
+	int count;
+public:
+	SP(T* temp) {
+		count = 0;
+		p = temp;
+	}
+	SP(const SP& temp) {
+		p = temp.p;
+		count++;
+	}
+	~SP() {
+		if (count == 0) {
+			delete p;
+		}
+		else {
+			count--;
+		}
+	}
+	SP operator =(const SP& temp) {
+		p = temp.p;
+		count++;
+		return *this;
+	}
+	T* operator->() {
+		return p;
+	}
+	T& operator*() {
+		return *p;
+	}
+
+
+};
